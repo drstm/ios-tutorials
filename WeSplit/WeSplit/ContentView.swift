b8c7918d24e4cc555adf371e5fa8aa8ddca2b8fd
Projjol <projjol@nyu.edu>
2020-03-28 13:53:17 -0400

    add in WESplit

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,66 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Projjol Banerji on 3/27/20.
+//  Copyright © 2020 Projjol Banerji. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = (orderAmount/100) * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal/peopleCount
+        return amountPerPerson
+    }
+    
+    var body: some View {
+        NavigationView{
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople){
+                        ForEach(2..<100){number in
+                            Text("\(number) people")
+                        }
+                    }
+                }
+                
+                Section(header: Text("How much tip would you like to leave?")) {
+                    Picker("Tip percentage", selection: $tipPercentage){
+                        ForEach(0 ..< tipPercentages.count){
+                            Text("\(self.tipPercentages[$0]) %")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                
+                Section(header: Text("Amount per person")) {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+            }
+            .navigationBarTitle("WeSplit")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
